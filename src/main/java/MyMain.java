6206e6f756bff7e6f1bde425fb83f970d68b50c9
Zara Alam <24zalam@athenian.org>
2021-09-15 07:06:30 -0700

    pennsylvania is spelled incorrectly on the libery bell

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,4 +1,31 @@
 import java.util.Scanner;
+//How do people represent numbers?              How do computers represent numbers?
+//People use decimal (base 10)                  Electrical circuits either 'off' or 'on'
+//We have 10 digits 0-9                         So it's easier to use binary: 0 and 1
+//                                              Base 2: two digits
+// 0,1,2,3,4,5,6,7,8,9                          0,1,10,11,100,101,110,111,1000,1001
+//Converting from binary to decimal:
+//123: one ten hundred place(1*100+2*10+3*1)    11010 --> (1*16+1*8+0*4+1*2+0*1)--go right to left
+
+//Convert Decimal to binary
+//Keep dividing by 2
+//26/2=13R0
+//13/2=6R1
+//6/2=3R0
+//3/2=1R1
+//2/1=0R1
+//11010
+
+
+
+
+
+
+
+
+
+
+
 
 public class MyMain {
 
@@ -8,17 +35,22 @@ public class MyMain {
     // Ex. binaryToDecimal("1010") => 10 (ten)
     //     binaryToDecimal("110010") => 50
     public static int binaryToDecimal(String binary) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int dec = Integer.parseInt(binary, 2);
+//while dec/2!=0
+//
+        return dec;
+
     }
+
+
 
     // Given a decimal number (e.g. 152) as input, and returns a String
     // representing the binary representation of that number.
     // Ex. decimalToBinary(7) => "111"
     //     decimalToBinary(152) => "10011000"
     public static String decimalToBinary(int decimal) {
-        // REPLACE WITH YOUR CODE
-        return "";
+        String binary = Integer.toBinaryString(decimal);
+        return binary;
     }
     
     
